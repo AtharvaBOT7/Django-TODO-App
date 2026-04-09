a4a9a5157a51c7296a7c11ead0b8103101345d52
atharvaBOT7 <atharvachundurwar841@gmail.com>
2026-04-08 20:04:02 -0700

    views code fixed

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Task
 
 def addTask(request):
+    task = request.POST['task']
+    Task.objects.create(task=task)
     return HttpResponse("Add Task")
